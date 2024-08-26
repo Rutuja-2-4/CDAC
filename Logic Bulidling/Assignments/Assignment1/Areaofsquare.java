d9c396dfec247e273d367371c3e1c863fb4e3f19
RUTUJA <131529312+Rutuja-2-4@users.noreply.github.com>
2024-08-26 18:13:45 +0530

    Add files via upload

---
+++ Logic Bulidling/Assignments/Assignment1/Areaofsquare.java
@@ -0,0 +1,9 @@
+ /*Next Step: Write a Java program that calculates the area of a square using the formula 
+area = side * side. Use a predefined side length.*/
+class Areaofsquare{
+	public static void main(String args[]){
+		int side = 5;
+		int Area =side*side;
+		System.out.println("Area of square is "+Area);
+	}
+}
