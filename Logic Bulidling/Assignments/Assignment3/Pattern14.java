21d5c576516a7122f6c6734b316d1ab7e9df0c25
RUTUJA <131529312+Rutuja-2-4@users.noreply.github.com>
2024-08-26 18:44:51 +0530

    Add files via upload

---
+++ Logic Bulidling/Assignments/Assignment3/Pattern14.java
@@ -0,0 +1,23 @@
+/*14. Write a program to print the following pattern: 
+* 
+** 
+*** 
+***** 
+******* 
+*********
+*/
+
+class Pattern14{
+	public static void main(String args[]){
+	int n =6;
+	{
+		for(int i=1;i<=n;i++){
+			for(int j=1;j<=i;j++){
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
+}
