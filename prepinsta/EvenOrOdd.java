43af0dd18bc8e1cf540f80bccb01b2deb14d36f1
RUTUJA <131529312+Rutuja-2-4@users.noreply.github.com>
2024-09-19 15:05:20 +0530

    Add files via upload

---
+++ prepinsta/EvenOrOdd.java
@@ -0,0 +1,16 @@
+
+//Check Whether a number is Even or Odd using Java 
+import java.util.*;
+public class EvenOrOdd {
+    public static void main(String [] args){
+Scanner sc = new Scanner(System.in);
+System.out.println("enter the the number");
+int number = sc.nextInt();
+if(number % 2 ==0){
+    System.out.println("Number is Even");
+}else{
+    System.out.println("number is odd");
+}
+    }
+    
+}
---
+++ prepinsta/sumOfNaturalNumbers.java
@@ -0,0 +1,15 @@
+//Find the Sum of First N Natural Numbers in Java
+public class sumOfNaturalNumbers {
+    public static void main(String args[]){
+        int n = 11; 
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum = sum + i;
+            }
+            System.out.println("Sum of first " + n + " natural numbers is " + sum);
+            
+       
+    
+
+    }
+}
