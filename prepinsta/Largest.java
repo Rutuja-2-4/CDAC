23b25af366faa64044eb89a2ba040f1434d54688
RUTUJA <131529312+Rutuja-2-4@users.noreply.github.com>
2024-09-26 20:24:50 +0530

    Add files via upload

---
+++ prepinsta/Largest.java
@@ -0,0 +1,14 @@
+// Write a program to find the largest of two numbers in java
+public class Largest{
+    public static void main(String[] args){
+        int num1 = 24;
+        int num2 =60;
+        if(num1 == num2){
+        System.out.println("the number are eaqual");
+        }else if(num1<num2){
+            System.out.println(num1 + " number is smaller");
+        } else{
+            System.out.println(num2 +"  number is greater");
+        }
+    }
+}
