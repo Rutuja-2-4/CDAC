5efb881d5d445885062ccd9c768d4ea2fac6d91c
RUTUJA <131529312+Rutuja-2-4@users.noreply.github.com>
2024-10-02 19:46:41 +0530

    Add files via upload

---
+++ ADS/Assignments/Assignment2/StringPalindrom.java
@@ -0,0 +1,18 @@
+public class StringPalindrom {
+    static String reverseString(String str){
+		int e = str.length() - 1;
+		String temp = "";
+		while(e >= 0) {
+			temp = temp+str.charAt(e);
+			e--;
+		}
+		return temp;
+	}
+    static boolean isPalindrome(String s){
+        return s.equals(reverseString(s));
+    }
+	public static void main(String[] args) {
+		String s = "helleh";
+		System.out.println(isPalindrome(s));
+	}
+}
